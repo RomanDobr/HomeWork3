5bfe62f78ce4d7b7216c5617a40da77e000763d7
Roman Dobrokhotov <dobro-ra@yandex.ru>
2023-12-12 22:22:22 +0300

    HomeWork3 задача 4-5 v.2

--- src/city/City.java
+++ src/city/City.java
@@ -7,13 +7,21 @@ public final class City {
     private final String nameCity;
     private final House[] houses;
 
+    private final House[] housesCity;
+
     public City(String nameCity, House[] houses) {
         this.nameCity = nameCity;
         this.houses = houses;
+        housesCity = new House[houses.length];
+        for (int i = 0; i < houses.length; i++) {
+           int number =  houses[i].getNumberHouse();
+           String name = houses[i].getStreet();
+           housesCity[i] = new House(name, number);
+        }
     }
 
     @Override
     public String toString() {
-        return "город :" + nameCity + ", дома :" + Arrays.toString(houses);
+        return "город :" + nameCity + ", дома :" + Arrays.toString(housesCity);
     }
 }
